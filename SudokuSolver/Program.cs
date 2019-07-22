c00bb01b786f877d299277ccf716b954f800701e
Patrick <revmydev@gmail.com>
2019-07-22 14:38:40 -0700

    building out Program

--- SudokuSolver/Program.cs
+++ SudokuSolver/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using SudokuSolver.Workers;
+using SudokuSolver.Strategies;
 
 namespace SudokuSolver
 {
@@ -10,6 +8,34 @@ namespace SudokuSolver
     {
         static void Main(string[] args)
         {
+            try
+            {
+                SudokuMapper sudokuMapper = new SudokuMapper();
+                SudokuBoardStateManager sudokuBoardStateManager = new SudokuBoardStateManager();
+                SudokuSolverEngine sudokuSolverEngine = new SudokuSolverEngine(sudokuBoardStateManager, sudokuMapper);
+                SudokuFileReader sudokuFileReader = new SudokuFileReader();
+                SudokuBoardDisplayer sudokuBoardDisplayer = new SudokuBoardDisplayer();
+
+                Console.WriteLine("Please enter the filename containing the sudoku puzzle: ");
+                var filename = Console.ReadLine();
+
+                var sudokuBoard = sudokuFileReader.ReadFile(filename);
+                sudokuBoardDisplayer.Display("Initial State", sudokuBoard);
+
+                bool isSudokuSolved = sudokuSolverEngine.Solve(sudokuBoard);
+                sudokuBoardDisplayer.Display("Final State", sudokuBoard);
+
+                Console.WriteLine(isSudokuSolved
+                    ? "You have successfully solved this sudoku puzzle!"
+                    : "Unfortunately, the current algorithms weren't enough to solve the puzzle.");
+
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("{0} : {1}", "Sudoku puzzle cannot be solved becasue there was an error: ", ex.Message);
+            }
         }
     }
 }
